851c4b46f355105843b9ac14a6f0b9097f0606d1
Vinh Nguyen <jvinhit@gmail.com>
2018-12-19 14:11:01 +0700

    a
    a

--- src/app.routings.js
+++ src/app.routings.js
@@ -1,115 +1,97 @@
-import React from 'react';
-import CountDown from './components/countdown';
-import ImageGallery from './components/imagegallery';
-import { CustomButton } from './components/common/custombutton';
-import { ToggleMenu } from './components/common/togglemenu';
-import { Menu } from './components/common/menu';
-import axios from 'axios';
-import Search from './components/searchbox';
-import TamGiacMathuat from './components/tamgiacmathuat';
-import Calculator from './components/liftingstateup';
-import LazyLoadimageComponent from './components/lazyloadimage';
+import React from "react";
+import CountDown from "./components/countdown";
+import ImageGallery from "./components/imagegallery";
+import { CustomButton } from "./components/common/custombutton";
+import { ToggleMenu } from "./components/common/togglemenu";
+import { Menu } from "./components/common/menu";
+import axios from "axios";
+import Search from "./components/searchbox";
+import TamGiacMathuat from "./components/tamgiacmathuat";
+import Calculator from "./components/liftingstateup";
+import LazyLoadimageComponent from "./components/lazyloadimage";
 const currentDate = new Date();
 const year =
-    currentDate.getMonth() === 11 && currentDate.getDate() > 23
-        ? currentDate.getFullYear() + 1
-        : currentDate.getFullYear();
+  currentDate.getMonth() === 11 && currentDate.getDate() > 23
+    ? currentDate.getFullYear() + 1
+    : currentDate.getFullYear();
 class RouteConfigs extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { isMenuHide: true };
-    }
-    onToggleMenu(e) {
-        e.stopPropagation();
-        e.preventDefault();
-        this.setState({ isMenuHide: !this.state.isMenuHide });
-        return;
-    }
-    componentDidMount() {
-        this.promiseUseCase()
-            .then(response => response.filter(item => item.userId === 1))
-            .then(dataUserId1 => console.log(dataUserId1))
-            .catch(err => console.log(err));
-    }
-    promiseUseCase() {
-        const url = 'https://jsonplaceholder.typicode.com/posts';
-        return new Promise((resolve, reject) => {
-            var a = new XMLHttpRequest();
-            a.onreadystatechange = function() {
-                if (a.readyState === a.DONE && a.status === 200) {
-                    const data = JSON.parse(a.response);
-                    resolve(data);
-                } else if (a.status === '404') {
-                    reject(a.status);
-                }
-            };
-            a.open('GET', url, true);
-            a.send();
-        });
-    }
+  constructor(props) {
+    super(props);
+    this.state = { isMenuHide: true };
+  }
+  onToggleMenu(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.setState({ isMenuHide: !this.state.isMenuHide });
+    return;
+  }
+  componentDidMount() {
+    this.promiseUseCase()
+      .then(response => response.filter(item => item.userId === 1))
+      .then(dataUserId1 => console.log(dataUserId1))
+      .catch(err => console.log(err));
+  }
+  promiseUseCase() {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    return new Promise((resolve, reject) => {
+      var a = new XMLHttpRequest();
+      a.onreadystatechange = function() {
+        if (a.readyState === a.DONE && a.status === 200) {
+          const data = JSON.parse(a.response);
+          resolve(data);
+        } else if (a.status === "404") {
+          reject(a.status);
+        }
+      };
+      a.open("GET", url, true);
+      a.send();
+    });
+  }
 
-    callApi(url, method) {
-        return new Promise((resolve, reject) => {
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-                    const data = JSON.parse(xhr.response);
-                    resolve(data);
-                } else if (xhr.status === '404') {
-                    reject(xhr.status);
-                }
-            };
-            xhr.open('GET', url, true);
-            xhr.send();
-        });
-    }
+  callApi(url, method) {
+    return new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+          const data = JSON.parse(xhr.response);
+          resolve(data);
+        } else if (xhr.status === "404") {
+          reject(xhr.status);
+        }
+      };
+      xhr.open("GET", url, true);
+      xhr.send();
+    });
+  }
 
-    render() {
-        return (
-            <div className="wrapper">
-                <div className="container">
-                    <CountDown date={`${year}-12-06T00:00:00`} />
-                    <ImageGallery infinite={true} autoplay={true} autoplaySpeed={1000}>
-                        <img src="https://salt.tikicdn.com/ts/lp/8a/81/e2/4fdaacbe3a3c3fccfd8cdfe29d868f9f.png" />
-                        <div style={{ paddingTop: '20px' }}>
-                            <span>This is html tag</span>
-                        </div>
-                        <img src="https://salt.tikicdn.com/ts/lp/53/c8/9b/22b09f264276653d69c0665ed525830f.png" />
-                        <div style={{ paddingTop: '20px' }}>
-                            <span>This is html tag seconds</span>
-                        </div>
-                        asd
-                    </ImageGallery>
-                    <div className="common">
-                        <div>
-                            <CustomButton classname="draw" text="Draw" />
-                            <CustomButton classname="meet draw" text="Draw Meet" />
-                            <CustomButton classname="meet draw spin" text="Center" />
-                            <CustomButton classname="meet draw" text="Spin" />
-                            <CustomButton classname="meet draw" text="Draw Meet" />
-                        </div>
-                    </div>
-                    <LazyLoadimageComponent />
-                    <div className="menu-container">
-                        <ToggleMenu onClick={e => this.onToggleMenu(e)}>
-                            <Menu
-                                isHide={this.state.isMenuHide}
-                                source={[
-                                    { link: '#home', text: 'Home' },
-                                    { link: '#home', text: 'Home' },
-                                    { link: '#home', text: 'Home' },
-                                    { link: '#home', text: 'Home' }
-                                ]}
-                            />
-                        </ToggleMenu>
-                        <div className="tam-giac">
-                            <TamGiacMathuat />
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="container">
+          <CountDown date={`${year}-12-06T00:00:00`} />
+          <ImageGallery infinite={true} autoplay={true} autoplaySpeed={1000}>
+            <img src="https://salt.tikicdn.com/ts/lp/8a/81/e2/4fdaacbe3a3c3fccfd8cdfe29d868f9f.png" />
+            <div style={{ paddingTop: "20px" }}>
+              <span>This is html tag</span>
             </div>
-        );
-    }
+            <img src="https://salt.tikicdn.com/ts/lp/53/c8/9b/22b09f264276653d69c0665ed525830f.png" />
+            <div style={{ paddingTop: "20px" }}>
+              <span>This is html tag seconds</span>
+            </div>
+            asd
+          </ImageGallery>
+          <div className="common">
+            <div style={{ "text-align": "center" }}>
+              <CustomButton classname="draw" text="Draw" />
+              <CustomButton classname="meet draw" text="Draw Meet" />
+            </div>
+          </div>
+          <LazyLoadimageComponent />
+          <div className="menu-container" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default RouteConfigs;
--- src/components/lazyloadimage/index.js
+++ src/components/lazyloadimage/index.js
@@ -1,68 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './index.scss';
-import { ImageItem } from './item/item';
-import { PostInput } from './postinput';
+import "./index.scss";
+import { ImageItem } from "./item/item";
+import { PostInput } from "./postinput";
 
 class LazyLoadimageComponent extends Component {
-    componentDidMount() {
-        var lazyloadImages = document.querySelectorAll('img.lazy');
-        var lazyloadThrottleTimeout;
+  componentDidMount() {
+    var lazyloadImages = document.querySelectorAll("img.lazy");
+    var lazyloadThrottleTimeout;
 
-        function lazyload() {
-            if (lazyloadThrottleTimeout) {
-                clearTimeout(lazyloadThrottleTimeout);
-            }
+    function lazyload() {
+      if (lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }
 
-            lazyloadThrottleTimeout = setTimeout(function() {
-                var scrollTop = window.pageYOffset;
-                lazyloadImages.forEach(function(img) {
-                    if (img.offsetTop < window.innerHeight + scrollTop) {
-                        img.src = img.dataset.src;
-
-                        img.style.opacity = 1;
-                        img.classList.remove('lazy');
-                    }
-                });
-                if (lazyloadImages.length == 0) {
-                    document.removeEventListener('scroll', lazyload);
-                }
-            }, 20);
-        }
-
-        document.addEventListener('scroll', lazyload);
-    }
-    onClickImage = e => {};
-    renderImageItem() {
-        console.log(this.props.imgSources);
-        return this.props.imgSources.map((item, index) => {
-            this.props.imgSources[index] = LazyLoadimageComponent.generateStaticImgSrc();
-            return (
-                <ImageItem
-                    slideClass={true}
-                    onClick={this.onClickImage}
-                    key={`${index}__`}
-                    dataSrc={this.props.imgSources[index]}
-                />
-            );
+      lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+          if (img.offsetTop < window.innerHeight + scrollTop) {
+            img.src = img.dataset.src;
+            img.onerror = function() {
+              img.src = "https://picsum.photos/600/600?random";
+            };
+            img.style.opacity = 1;
+            img.classList.remove("lazy");
+          }
         });
+        if (lazyloadImages.length == 0) {
+          document.removeEventListener("scroll", lazyload);
+        }
+      }, 20);
     }
-    render() {
-        return (
-            <div className="lazy-load-container">
-                <PostInput />
-                {this.renderImageItem()}
-            </div>
-        );
-    }
+
+    document.addEventListener("scroll", lazyload);
+  }
+  onClickImage = e => {};
+  renderImageItem() {
+    console.log(this.props.imgSources);
+    return this.props.imgSources.map((item, index) => {
+      this.props.imgSources[
+        index
+      ] = LazyLoadimageComponent.generateStaticImgSrc();
+      return (
+        <ImageItem
+          slideClass={true}
+          onClick={this.onClickImage}
+          key={`${index}__`}
+          dataSrc={this.props.imgSources[index]}
+        />
+      );
+    });
+  }
+  render() {
+    return (
+      <div className="lazy-load-container">
+        <PostInput />
+        {this.renderImageItem()}
+      </div>
+    );
+  }
 }
 const random = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 LazyLoadimageComponent.generateStaticImgSrc = () => {
-    return `https://picsum.photos/600/600/?image=${random(1, 1000)}`;
+  return `https://picsum.photos/600/600/?image=${random(1, 1000)}`;
 };
 LazyLoadimageComponent.defaultProps = {
-    imgSources: new Array(100).fill('')
+  imgSources: new Array(100).fill("")
 };
 export default LazyLoadimageComponent;
