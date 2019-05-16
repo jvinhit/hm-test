c2e9f97f93f2c512e1f849348da73cc37a93c220
Vĩnh Nguyễn <jvinhit@gmail.com>
2019-05-16 20:57:19 +0700

    .gitignore is now working

---
+++ build/precache-manifest.72562f57b0a260609f48f80488c3d7d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6bde0dfcb0bb4d59d67e",
+    "url": "/static/js/main.6bde0dfc.chunk.js"
+  },
+  {
+    "revision": "08e30e68015381b2e049",
+    "url": "/static/js/1.08e30e68.chunk.js"
+  },
+  {
+    "revision": "6bde0dfcb0bb4d59d67e",
+    "url": "/static/css/main.0ce5b599.chunk.css"
+  },
+  {
+    "revision": "4167dc4d2c32b168e27f00221c3a458b",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import { render } from 'react-dom';
 import RouteConfigs from './app.routings';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope, faKey, faImage, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
-
+import './styles.scss';
 library.add(faEnvelope, faKey, faImage, faPencilAlt);
 
-import './styles.scss';
+
 
 render(<RouteConfigs />, document.getElementById('root'));
